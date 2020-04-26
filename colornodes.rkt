79def06bfe26427e054ab0ee0efaf4894181d711
ethangreenberg <ethan_greenberg@brown.edu>
2020-04-26 12:39:47 -0400

    Added acyclic orientation and non-adj colors

--- colornodes.rkt
+++ colornodes.rkt
@@ -1,12 +1,13 @@
 #lang forge
 
 sig Node {
-    refs: set Node,
-    dists: set Node -> Int
+    refs: set Node, -- undirected graph
+    dists: set Node -> Int, -- distances from node to all other nodes
+    acOri: set Node -- acyclic orientation for the graph
 }
 
 sig Color {
-    nodeColors: set Node
+    nodeColors: set Node -- defines the color of each node
 }
 
 -- graph is undirected
@@ -23,7 +24,7 @@ pred irreflexive {
 pred oneColor {
     all n : Node | one n.(~nodeColors)
 }
-
+-- defines distance metric for each node
 pred defDists {
     all u : Node | all v : Node | {
         u = v => dists[u][v] = sing[0]
@@ -33,11 +34,29 @@ pred defDists {
     }
 }
 
+-- defines ori as an acyclic 
+pred defOrientation {
+    acOri in refs
+    all u: Node | {
+        -- acyclic
+        u->u not in ^acOri 
+         -- exactly one direction picked for each edge
+        all v : u.refs | (u->v in acOri iff v->u not in acOri)
+    }
+}
+
+-- defines that no two adjacent nodes should have the same color
+pred noAdjColors {
+    all n : Node | nodeColors.n not in nodeColors.(n.refs)
+}
+
 pred setup {
     symmetric
     irreflexive
     oneColor
     defDists
+    defOrientation
+    noAdjColors
 }
 
-run {setup} for exactly 4 Node, exactly 1 Color
+run {setup} for exactly 4 Node, exactly 2 Color
